e60abcdbc3eb19dbd70a79ed34399343e4c75473
HEHAO <102503359+heiansstm@users.noreply.github.com>
2023-09-11 20:21:51 +0800

    Update script.py

--- extensions/api/script.py
+++ extensions/api/script.py
@@ -8,6 +8,6 @@ from modules import shared
 def setup():
     blocking_api.start_server(shared.args.api_blocking_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
     if shared.args.public_api:
-        time.sleep(5)
+        time.sleep(20)
 
     streaming_api.start_server(shared.args.api_streaming_port, share=shared.args.public_api, tunnel_id=shared.args.public_api_id)
